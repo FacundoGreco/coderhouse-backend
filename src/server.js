d63ce1b8ff38971c8ff0fad06f959bf1e148e9dc
FacundoGreco <facundogrecor@gmail.com>
2021-11-13 10:54:25 -0300

    Implements EJS and creates simple index page

--- src/server.js
+++ src/server.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const { router: productsRouter } = require("./routers/apiProducts.js");
 const { router: cartsRouter } = require("./routers/apiCarts.js");
 
 //MIDDLEWARES
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 //ROUTERS
-// app.use(express.static("./public"));
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 
 //ROUTES
+app.get("/", (req, res) => {
+	res.render("pages/index", {});
+});
+
 app.all("*", (req, res) => {
 	res.status(404).json({ error: 404, descripción: `Route '${req.url}' and method '${req.method}' not implemented.` });
 });
